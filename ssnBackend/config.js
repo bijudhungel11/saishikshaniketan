5eff4c18e183dd24f5999c45af9fb44fa6923585
bijudhungel11 <65780598+bijudhungel11@users.noreply.github.com>
2021-06-11 20:39:14 +0545

    Delete config.js

--- ssnBackend/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  MONGODB_URL:
-    process.env.MONGODB_URL ||
-    "mongodb+srv://bijudhungel:maaA33fU6f8CaKUa@cluster0.5pemo.mongodb.net/<dbname>?retryWrites=true&w=majority",
-  JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
-};
-/* localhost one==>"mongodb://localhost/ssn" */
-/* clusters one==>mongodb+srv://bijudhungel:<password>@cluster0.5pemo.mongodb.net/<dbname>?retryWrites=true&w=majority */
